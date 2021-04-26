78ad3803a5b678ccc97deea6c1fd3cf416f92f58
Gioele <gioele.armiento@gmail.com>
2021-04-26 16:09:46 +0200

    Lorenzo Jumbotron added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 let mix = require('laravel-mix');
 mix
-    .sass('src/style.scss', 'css/')
+    .sass('src/style.scss', 'css/').options({
+        processCssUrls: false
+    })
     .setPublicPath('dist')
     .copy('src/index.html', 'dist')
 ;
